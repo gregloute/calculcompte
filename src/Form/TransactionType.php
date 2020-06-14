148e26ea72ea7c04aba8fa4dfc5b73ca1fe8ac62
gregloute <breteau.gregoire@gmail.com>
2020-06-14 16:06:16 +0200

    add transaction forms fr

--- src/Form/TransactionType.php
+++ src/Form/TransactionType.php
@@ -24,6 +24,7 @@ TransactionType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Transaction::class,
+            'translation_domain' => 'forms',
         ]);
     }
 }
